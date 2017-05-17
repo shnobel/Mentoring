5096b407485a17846f2703798135fcf52fa6763e
Dmitriy <dmytrovoronov@hotmail.com>
2017-05-17 03:58:51 +0300

    Refactoring for remove and removeAt + added prototype for ElementAt in linkedList

--- DataStructures/MyLinkedList/LinkedList.cs
+++ DataStructures/MyLinkedList/LinkedList.cs
@@ -8,9 +8,24 @@ namespace MyLinkedList
     {
         private ListNode<T> head;
         private ListNode<T> current;
+        private ListNode<T> counter;
 
         public int Length { get; private set; }
-
+        private ListNode<T> Counter
+        {
+            get
+            {
+                if(counter == null)
+                {
+                    return head.Next;
+                }
+                return counter;
+            }
+            set
+            {
+                counter = value;
+            }
+        }
         public void Add(T value)
         {
             var listNode = new ListNode<T>(value, current, null);
@@ -28,9 +43,10 @@ namespace MyLinkedList
 
         public void AddAt(T value, int position)
         {
-            if (position >= Length || position < 1) throw new IndexOutOfRangeException();
+            if (position >= Length || position < 0) throw new IndexOutOfRangeException();
 
-            var currentValueAtPosition = head.ElementAt(position);
+            var currentValueAtPosition = ElementAt(position);
+            //var currentValueAtPosition = head.ElementAt(position);
             var prevValue = currentValueAtPosition.Prev;
             var newValue = new ListNode<T>(value, prevValue, currentValueAtPosition);
             prevValue.Next = newValue;
@@ -40,11 +56,13 @@ namespace MyLinkedList
 
         public void Remove(T value)
         {
-            for(int index = 1; index <= Length; index++)
+            for(int index = 0; index <= Length; index++)
             {
-                if(head.ElementAt(index).Value.Equals(value))
+                var currentElement = ElementAt(index);
+                //var currentElement = head.ElementAt(index);
+                if (currentElement.Value.Equals(value))
                 {
-                    RemoveAt(index);
+                    DeleteElement(index, currentElement);
                     return;
                 }
             }
@@ -52,28 +70,51 @@ namespace MyLinkedList
 
         public void RemoveAt(int position)
         {
-            if (position < 1 || position > Length) throw new IndexOutOfRangeException();
+            var currentValueAtPosition = ElementAt(position);
+            //var currentValueAtPosition = head.ElementAt(position);
+            DeleteElement(position, currentValueAtPosition);
+        }
 
-           
-            if (position == 1)
+        private void DeleteElement(int position, ListNode<T> element)
+        {
+            if (position < 0 || position > Length) throw new IndexOutOfRangeException();
+            if (position == 0)
             {
                 head = head.Next;
             }
             else
             {
-                var currentValueAtPosition = head.ElementAt(position);
-                var prevValue = currentValueAtPosition.Prev;
-                var nextValue = currentValueAtPosition.Next;
+                var prevValue = element.Prev;
+                var nextValue = element.Next;
                 prevValue.Next = nextValue;
                 nextValue.Prev = prevValue;
             }
             Length--;
         }
 
-        public T ElementAt(int position)
+        //public T ElementAt(int position)
+        //{
+        //    if (position < 1 || position > Length) throw new IndexOutOfRangeException();
+        //    return head.ElementAt(position).Value;
+        //}
+
+        public ListNode<T> ElementAt(int position)
         {
-            if (position < 1 || position > Length) throw new IndexOutOfRangeException();
-            return head.ElementAt(position).Value;
+            if (position < 0 || position > Length) throw new IndexOutOfRangeException();
+            ListNode<T> result;
+            //if (position == 1) result = this;
+            if (position == 0) result = Counter;
+            else if (position < 0)
+            {
+                result = null;
+            }
+            else
+            {
+                Counter = Counter.Next;
+                result = ElementAt(position - 1);
+                //result = Next.ElementAt(position - 1);
+            }
+            return result;
         }
 
         public IEnumerator<T> GetEnumerator()
--- DataStructures/MyLinkedList/ListNode.cs
+++ DataStructures/MyLinkedList/ListNode.cs
@@ -17,20 +17,20 @@
 
         public ListNode(T value) : this(value, null, null){}
 
-        public ListNode<T> ElementAt(int position)
-        {
-            ListNode<T> result;
-            if (position == 1) result = this;
-            else if (position < 1 )
-            {
-                result = null;
-            }
-            else
-            {
-                result = Next.ElementAt(position - 1);
-            }
-            return result;
-        }
+        //public ListNode<T> ElementAt(int position)
+        //{
+        //    ListNode<T> result;
+        //    if (position == 1) result = this;
+        //    else if (position < 1 )
+        //    {
+        //        result = null;
+        //    }
+        //    else
+        //    {
+        //        result = Next.ElementAt(position - 1);
+        //    }
+        //    return result;
+        //}
 
     }
 }
